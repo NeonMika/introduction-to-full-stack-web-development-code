7009de66c450b5fe71eab731b78288b41c206118
Dr. Markus Weninger <markus.weninger@jku.at>
2024-05-06 10:58:46 +0200

    Rework auth code a bit to give more detailed output on server

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.get(["/login", "/register"], function(req, res, next) {
   res.sendFile(path.join(__dirname, 'public', 'login.html'));
 });
 
-function verifyToken(req, res, next) {
+function verifyRequest(req) {
     let token = req.headers['authorization'];
     req.jwtProvided = false;
     req.jwtVerifyError = false;
@@ -34,6 +34,7 @@ function verifyToken(req, res, next) {
     req.jwtPayload = null;
 
     if (token) {
+        console.log(`> Authorization: Token "${token}" provided as Authorization-header`)
         token = token.replace("Bearer ", "")
         req.jwtProvided = true;
         jwt.verify(token, jwtSecret, (err, decoded) => {
@@ -47,18 +48,32 @@ function verifyToken(req, res, next) {
                 // console.log("JWT: ", decoded)
                 req.jwtPayload = decoded;
             }
-            next();
         });
     } else {
-        next();
+        console.log("> Authorization: No token provided as Authorization-header")
     }
 }
 
-// Apply the verifyToken middleware to all routes
+function verifyMiddleware(req, res, next) {
+  console.log(`Verify token on request to ${req.url}`)
+  verifyRequest(req)
+  if(!req.jwtProvided) {
+    console.log(`>>> Not authorized, no token provided`)
+  } else if(req.jwtProvided && !req.jwtVerifyError) {
+    console.log(`>>> Authorized`)
+  } else if(req.jwtProvided && req.jwtVerifyError && req.jwtExpired) {
+    console.log(`>>> Not authorized, token expired`)
+  } else {
+    console.log(`>>> Not authorized, error during token verification`)
+  }
+  next()
+}
+
+// Apply the verifyMiddleware middleware to all routes
 // The order of our middlewares matters: If we put this before
 // setting up our express.static middleware, JWT would even be checked
 // for every static resource, which would introduce unncessary overhead.
-app.use(verifyToken);
+app.use(verifyMiddleware);
 
 // handles /user_handling/login and /user_handling/register
 app.use('/user_handling', userRouter);
